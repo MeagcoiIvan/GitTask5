c5600975bdee0f53233529636eaa6ef4a3d923b9
MeagcoiIvan <meagcoi.ivan@gmail.com>
2020-10-28 15:12:50 +0200

    add all methods 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public void method3(){}
     public void method1(){}
     public void method2(){}
 }
